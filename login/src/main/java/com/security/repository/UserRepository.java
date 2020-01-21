a5265a800f6ac6a95d2954c5fc6ee4bc3010babb
MartinCode <julio.luque@gmail.com>
2020-01-21 06:50:48 -0500

    Login con eclipse

---
+++ login/src/main/java/com/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.security.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.security.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>  {
+    public Optional<User> findByUsername(String username);
+}
